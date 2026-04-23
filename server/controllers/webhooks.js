23ebcb9b48d7ad55576e440a3fcb2679d09e6a33
Saipreety Sutar <saipreetysutar2315@gmail.com>
2026-04-23 12:52:16 +0530

    corrected webhooks again

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -7,7 +7,7 @@ const clerkWebhooks = async (req, res) => {
    try {
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
-    await whook.verify(json.stringify(req.body), {
+    await whook.verify(JSON.stringify(req.body), {
         "svix_id": req.headers['svix-id'],
         "svix-timestamp": req.headers['svix-timestamp'],
         "svix-signature": req.headers['svix-signature'],
