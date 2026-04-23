19d25a24042b89c6d2c1ce205e9318034b5b600f
Saipreety Sutar <saipreetysutar2315@gmail.com>
2026-04-23 11:48:22 +0530

    Fixed server folder

---
+++ server/controllers/educatorContoller.js
@@ -0,0 +1,20 @@
+import { clerkClient } from '@clerk/express'
+
+
+// update role to educator
+export const udpateRoleEducator = async () => {
+    try {
+        const userId = req.auth.userId
+
+        await clerkClient.users.updateUserMetadata(userId, {
+            publicMetadata:{
+                role: 'educator',
+            }
+        })
+
+        res.json({ success: true, message: "You can publish a course now" })
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
