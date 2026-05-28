74a794f1b1087e77c69bae2489796d6c682cb318
saipreety sutar <saipreetysutar2315@gmail.com>
2026-05-28 19:46:47 +0530

    Enabled open cors

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,7 @@ const app = express();
 const PORT = 4000;
 
 // Middleware
-app.use(cors({
-    origin: ['https://learning-management-system-q3jkc81h6.vercel.app','https://learning-management-system-dgz7-opsd0gte9.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-}));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(clerkMiddleware());
